2e4ad0737b9724f2cdd125d6cb2f347a9e1edb3c
Webdgepl <cholewski185@gmail.com>
2022-03-06 17:17:37 +0100

    Fix app scss

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./App.module.scss";
+import DishForm from "./Form/DishForm";
+
+function App() {
+	return (
+		<main>
+			<section className={styles.app}>
+				<DishForm />
+			</section>
+		</main>
+	);
+}
+
+export default App;
